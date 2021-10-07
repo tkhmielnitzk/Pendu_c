1be540ca44ad2a333fc065214f382ecd1bc07125
Thomas Khmielnitzky <tkhmielnitzky@gmail.com>
2021-10-07 16:50:49 +0200

    ooo

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // insert code here... ajout inutile
     printf("Bonjour, World!!, ca va ?\n");
 
     return 0;
